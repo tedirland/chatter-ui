ffc537bde45171d72d004b106ec7aeb63b83b91d
tedirland <irlandth@gmail.com>
2025-04-21 08:28:33 -0500

    Boilerplate for new Chat React + Nest Chat app using Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
+import {
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+} from '@mui/material';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// setting a global theme based on the mui API
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container>
+        <h1>Dark Mode</h1>
+      </Container>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
